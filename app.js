d4a71e3f8930a48b5e9813389a1c43a4b0a6c8bb
jerome-shiftleft <jerome@theshiftleft.com>
2023-11-18 03:30:01 +0800

    methods progress

--- app.js
+++ app.js
@@ -4,7 +4,18 @@ const app = Vue.createApp({
       courseGoal: 'Finish the course and learn Vue!',
       vueLink: 'https://vuejs.org'
     }
+  },
+  methods: {
+    outputGoal: () => {
+      const randomNumber = Math.random();
+      console.log('random number: ', randomNumber);
+      if (randomNumber < 0.5) {
+        return 'Learn Vue!';
+      } else {
+        return 'Master Vue!';
+      }
+    }
   }
-});
+}); // end of const app = Vue.createApp({
 
 app.mount('#user-goal');
