9c3d3c4d0729abc71b95ca25d23bf0ddaf53c659
jerome-shiftleft <jerome@theshiftleft.com>
2023-11-19 02:06:58 +0800

    two-way-binding starting files

--- app.js
+++ app.js
@@ -1,30 +1,20 @@
 const app = Vue.createApp({
   data() {
     return {
-      counter: 10,
-      name: '',
-      confirmedName: ''
+      counter: 0,
+      name: ''
     };
   },
   methods: {
-    setName(event, lastname) {
-      this.name = event.target.value + lastname;
-      console.log(`name: ${this.name}`);
+    setName(event, lastName) {
+      this.name = event.target.value + ' ' + lastName;
     },
-    confirmInput() {
-      console.log('confirming input!');
-      this.confirmedName = this.name;
-      console.log(`confirmed name: ${this.confirmedName}`);
-    },
-    submitForm() {
-      //event.preventDefault();
-      console.log('submitting form!');
-    },
-    add(num = 1) {
+    add(num) {
       this.counter = this.counter + num;
     },
-    reduce(num = 1) {
+    reduce(num) {
       this.counter = this.counter - num;
+      // this.counter--;
     }
   }
 });
