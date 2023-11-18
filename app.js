568bf2ebc9387170cee28f5368fb4f7a18ee617e
jerome-shiftleft <jerome@theshiftleft.com>
2023-11-18 21:56:37 +0800

    native event object

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: ''
     };
   },
   methods: {
+    setName(event, lastname) {
+      this.name = event.target.value + lastname;
+    },
     add(num = 1) {
       this.counter = this.counter + num;
     },
