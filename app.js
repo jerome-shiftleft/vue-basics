7960e6135a53c56815e72568170212848e677f9b
jerome-shiftleft <jerome@theshiftleft.com>
2023-11-19 05:29:52 +0800

    progress

---
+++ app.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      boxASelected: false,
+      boxBSelected: false,
+      boxCSelected: false
+    }
+  },
+  methods: {
+    boxSelected(box) {
+      
+    }
+  }
+});
+
+app.mount('#styling');
