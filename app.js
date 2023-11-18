3f3473ed43798dcd84dc1dbe9c03db4da81e52d3
jerome-shiftleft <jerome@theshiftleft.com>
2023-11-19 00:27:46 +0800

    event modifiers

--- app.js
+++ app.js
@@ -2,16 +2,23 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      confirmedName: ''
     };
   },
   methods: {
     setName(event, lastname) {
       this.name = event.target.value + lastname;
+      console.log(`name: ${this.name}`);
+    },
+    confirmInput() {
+      console.log('confirming input!');
+      this.confirmedName = this.name;
+      console.log(`confirmed name: ${this.confirmedName}`);
     },
     submitForm() {
       //event.preventDefault();
-      console.log('submitting form');
+      console.log('submitting form!');
     },
     add(num = 1) {
       this.counter = this.counter + num;
