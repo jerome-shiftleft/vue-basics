0315cf6dd4ea596fefd3561b81ae1fbf3803896e
jerome-shiftleft <jerome@theshiftleft.com>
2023-11-18 20:22:07 +0800

    event starting files

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoalA: '<span>Finish the course and learn Vue!</span>',
-      courseGoalB: '<span>Master Vue and build amazing apps!</span>',
-      vueLink: 'https://vuejs.org'
-    }
+      counter: 0,
+    };
   },
-  methods: {
-    outputGoal() {
-      const randomNumber = Math.random();
-      console.log('random number: ', randomNumber);      
-      return (randomNumber < 0.5) ? this.courseGoalA : this.courseGoalB;
-    }
-  }
-}); // end of const app = Vue.createApp({
+});
 
-app.mount('#user-goal');
+app.mount('#events');
