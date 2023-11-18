bb4995ce6cc2268c1434a511a3308c015cc36b5f
jerome-shiftleft <jerome@theshiftleft.com>
2023-11-18 20:52:41 +0800

    event methods

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const app = Vue.createApp({
     return {
       counter: 0,
     };
+  },
+  methods: {
+    add() {
+      this.counter++
+    },
+    reduce() {
+      this.counter--;
+    }
   }
 });
 
