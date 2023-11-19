2f41dc44d3d815229d58d5bf593206825b1681eb
jerome-shiftleft <jerome@theshiftleft.com>
2023-11-19 17:06:03 +0800

    progress

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return {
+      goal: '',
+      goals: ['Finish the course!'],      
+    };
   },
+  methods: {
+    addGoal() {
+      console.log('adding goal!');
+      this.goals.push(this.goal);
+      console.log(this.goals);
+    }
+  }
 });
 
 app.mount('#user-goals');
