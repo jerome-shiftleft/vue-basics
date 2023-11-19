7d372ee5bff76c7663308d144ae67634656acc23
jerome-shiftleft <jerome@theshiftleft.com>
2023-11-19 10:02:15 +0800

    rendering conditional content and lists starting files

--- app.js
+++ app.js
@@ -1,28 +1,7 @@
 const app = Vue.createApp({
   data() {
-    return {
-      boxASelected: false,
-      boxBSelected: false,
-      boxCSelected: false
-    }
+    return { goals: [] };
   },
-  computed: {
-    boxAClasses() {
-      return { active: this.boxASelected};
-    }
-  },
-  methods: {
-    boxSelected(box) {
-      console.log(`box ${box} selected!`);
-      if (box === 'A') {
-        this.boxASelected = !this.boxASelected;
-      } else if (box === 'B') {
-        this.boxBSelected = !this.boxBSelected;
-      } else if (box === 'C') {
-        this.boxCSelected = !this.boxCSelected;
-      }
-    }
-  }
 });
 
-app.mount('#styling');
+app.mount('#user-goals');
